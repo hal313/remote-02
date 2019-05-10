e86be3ea65aed2493243dcade5b433b81c40eb39
John Ghidiu <john-github@rmdashrf.org>
2019-05-10 18:49:45 -0400

    Initial deposit

---
+++ app/index.php
@@ -0,0 +1,6 @@
+<?php
+    header('Content-type: application/json');
+    header('Access-Control-Allow-Origin: *');
+    $reverseString = strrev($_GET['message']);
+    echo "{\"message\": \"${reverseString}\"}";
+?>
